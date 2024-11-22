fccd277cf46029188852c894f63418de0948ef7f
Neto test <eprohacev@gmail.com>
2024-11-22 16:40:36 +0300

    first commit

---
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/MainActivityTest.java
@@ -0,0 +1,72 @@
+package ru.kkuzmichev.simpleappforespresso;
+
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasAction;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasData;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+import android.content.Intent;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.intent.Intents;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.filters.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import io.qameta.allure.android.runners.AllureAndroidJUnit4;
+
+@LargeTest
+@RunWith(AllureAndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+//    public IntentsTestRule intentsTestRule =
+//            new IntentsTestRule(MainActivity.class);
+    public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void mainActivityTest() {
+        ViewInteraction element = onView(withContentDescription("More options"));
+        element.check(matches(isDisplayed()));
+        element.perform(click());
+
+        ViewInteraction settingsBut = onView(withId(R.id.content));
+        settingsBut.check(matches(isDisplayed()));
+        Intents.init();
+        settingsBut.perform(click());
+
+        intended(hasData("https://google.com"));
+        intended(hasAction(Intent.ACTION_VIEW));
+        Intents.release();
+
+    }
+
+//    private static Matcher<View> childAtPosition(
+//            final Matcher<View> parentMatcher, final int position) {
+//
+//        return new TypeSafeMatcher<View>() {
+//            @Override
+//            public void describeTo(Description description) {
+//                description.appendText("Child at position " + position + " in parent ");
+//                parentMatcher.describeTo(description);
+//            }
+//
+//            @Override
+//            public boolean matchesSafely(View view) {
+//                ViewParent parent = view.getParent();
+//                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+//                        && view.equals(((ViewGroup) parent).getChildAt(position));
+//            }
+//        };
+//    }
+}
